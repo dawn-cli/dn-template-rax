aa697dacb2934222e1408271a7d4a46490db178d
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2017-09-25 23:58:12 +0800

    0.0.3

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (webpackConf) {
-  webpackConf.resolve = webpackConf.resolve || {};
-  webpackConf.resolve.alias = webpackConf.resolve.alias || {};
-  Object.assign(webpackConf.resolve.alias, {
-    'react': 'rax',
-    'react-dom': 'rax-dom',
-    'create-react-class': "rax/lib/createClass"
-  });
-};
